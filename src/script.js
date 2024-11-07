44d1cbe7b9cd582bbea5d7c281b3ebcdc2f8f6b7
Kshitij033 <kshitij.tripathilko69@gmail.com>
2024-11-07 14:29:51 +0530

    weather cards function

--- src/script.js
+++ src/script.js
@@ -2,14 +2,52 @@ import api_key from './apikey.js';
 
 const search = document.querySelector("#search");
 const place = document.querySelector("#place");
+const cardArea = document.querySelector("#weathercards");
+
+
+const weatherCard = (element)=>{
+    const card = document.createElement("li");
+    card.id = "card";
+    card.classList.add("bg-slate-500", "list-none", "text-white","py-5", "px-4","rounded","min-w-48","max-w-52","w-1/5","flex-wrap");
+    card.innerHTML =  `<h3 class="font-bold text-2xl">${element.dt_txt.split(" ")[0]}</h3>
+                <img src="icons/${element.weather[0].icon}.png" class="w-14 max-w-16 mt-1.5" alt="weather-icon">
+                <h4 class="font-medium mt-3">Temp: ${element.main.temp} °C</h4>
+                <h4 class="font-medium mt-3">Wind: ${element.wind.speed} M/S</h4>
+                <h4 class="font-medium mt-3">Humidity: ${element.main.humidity}%</h4>`;
+    cardArea.appendChild(card);
+};
+
 
 
 const weatherDetails = (lat,lon, pname) => {
-    const URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
+    const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`;
 
     fetch(URL)
     .then(results=>results.json())
     .then(result => {
+        console.log(result);
+
+
+        const collectDays= [];
+        const batchForecast = result.list.filter(terms => {
+            const date = new Date(terms.dt_txt);
+            const day = date.getDate();
+            const hour = date.getHours();
+            if(!collectDays.includes(day) && (hour === 9)){
+                collectDays.push(day) ;
+                return true;
+            }
+            return false;
+        })
+        console.log(collectDays)
+        console.log(batchForecast)
+        
+        place.value="";
+        cardArea.innerHTML= "";
+
+        batchForecast.forEach(element => {
+            weatherCard(element);
+        });
 
     }).catch(error => alert(`Error:(${error})`))
 
@@ -18,7 +56,7 @@ const weatherDetails = (lat,lon, pname) => {
 const getGeolocation = () => {
     const pname = place.value.trim();
     if(!pname) return;
-    const URL = `http://api.openweathermap.org/geo/1.0/direct?q=${pname}&limit=1&appid=${api_key}`;
+    const URL = `https://api.openweathermap.org/geo/1.0/direct?q=${pname}&limit=1&appid=${api_key}`;
 
     fetch(URL)
     .then(results => results.json())
