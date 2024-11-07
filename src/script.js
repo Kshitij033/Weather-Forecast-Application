87a41c4f65a3ed2b7c301f9a13b322bc415d31cc
Kshitij033 <kshitij.tripathilko69@gmail.com>
2024-11-07 18:26:18 +0530

    default dom loading

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ const weatherdata = document.querySelector("#weatherdata");
 const currentWeatherCard = (element,pname)=>{
     const details = document.createElement("div");
     details.id = "details";
-    details.innerHTML = `<h2 class="font-bold text-2xl pr-5 min-w-40">${pname} ${element.dt_txt.split(" ")[0]}</h2>
+    details.innerHTML = `<h2 class="font-bold text-2xl pr-5 min-w-40">${pname} (${element.dt_txt.split(" ")[0]})</h2>
                         <h4 class="font-medium mt-3">Temperature: ${element.main.temp} °C</h4>
                         <h4 class="font-medium mt-3">Wind: ${element.wind.speed} M/S</h4>
                         <h4 class="font-medium mt-3">Humidity: ${element.main.humidity}%</h4>`
@@ -29,9 +29,9 @@ const currentWeatherCard = (element,pname)=>{
 const weatherCard = (element)=>{
     const card = document.createElement("li");
     card.id = "card";
-    card.classList.add("bg-slate-500", "list-none", "text-white","py-5", "px-4","rounded","min-w-49", "max-[900px]:min-w-[11.5rem]", "max-[478px]:min-w-[12rem]", "max-[460px]:min-w-[11rem]", "max-[428px]:min-w-[10rem]", "max-[400px]:min-w-[9rem]", "max-[384px]:min-w-[8rem]", "max-[900px]:p-3.5", "max-[400px]:p-3", "max-[384px]:p-2","flex-wrap");
+    card.classList.add("bg-slate-500", "list-none", "text-white","py-5", "px-4", "text-center", "rounded","min-w-49", "max-[900px]:min-w-[11.5rem]", "max-[478px]:min-w-[12rem]", "max-[460px]:min-w-[11rem]", "max-[428px]:min-w-[10rem]", "max-[400px]:min-w-[9rem]", "max-[384px]:min-w-[8rem]", "max-[900px]:p-3.5", "max-[400px]:p-3", "max-[384px]:p-2","flex-wrap");
     card.innerHTML =  `<h3 class="font-bold text-2xl">${element.dt_txt.split(" ")[0]}</h3>
-                <img src="icons/${element.weather[0].icon}.png" class="w-14 max-w-16 mt-1.5 drop-shadow-[0_0_10px_rgba(255,255,255,0.6)]" alt="weather-icon">
+                <img src="icons/${element.weather[0].icon}.png" class="w-14 max-w-16 mt-1.5 inline drop-shadow-[0_0_10px_rgba(255,255,255,0.6)]" alt="weather-icon">
                 <h4 class="font-medium mt-3">Temp: ${element.main.temp} °C</h4>
                 <h4 class="font-medium mt-3">Wind: ${element.wind.speed} M/S</h4>
                 <h4 class="font-medium mt-3">Humidity: ${element.main.humidity}%</h4>`;
@@ -96,3 +96,10 @@ const getGeolocation = () => {
 }
 
 search.addEventListener("click", getGeolocation);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const defaultCity = "Jakarta";
+    const lat = -6.2088;
+    const lon = 106.8456; 
+    weatherDetails(lat, lon, defaultCity);   
+});
