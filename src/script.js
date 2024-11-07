725bcb74513cfe9eb7460f11c4bcd79bd18750b8
Kshitij033 <kshitij.tripathilko69@gmail.com>
2024-11-07 16:46:21 +0530

    fixed responsiveness for <900px

--- src/script.js
+++ src/script.js
@@ -8,7 +8,7 @@ const cardArea = document.querySelector("#weathercards");
 const weatherCard = (element)=>{
     const card = document.createElement("li");
     card.id = "card";
-    card.classList.add("bg-slate-500", "list-none", "text-white","py-5", "px-4","rounded","min-w-49", "max-[900px]:min-w-[11.5rem]", "max-[478px]:min-w-[12rem]", "max-[460px]:min-w-[11rem]", "max-[428px]:min-w-[10rem]", "max-[400px]:min-w-[9rem]", "max-[384px]:min-w-[8rem]","max-[400px]:p-3", "max-[384px]:p-2","flex-wrap");
+    card.classList.add("bg-slate-500", "list-none", "text-white","py-5", "px-4","rounded","min-w-49", "max-[900px]:min-w-[11.5rem]", "max-[478px]:min-w-[12rem]", "max-[460px]:min-w-[11rem]", "max-[428px]:min-w-[10rem]", "max-[400px]:min-w-[9rem]", "max-[384px]:min-w-[8rem]", "max-[900px]:p-3.5", "max-[400px]:p-3", "max-[384px]:p-2","flex-wrap");
     card.innerHTML =  `<h3 class="font-bold text-2xl">${element.dt_txt.split(" ")[0]}</h3>
                 <img src="icons/${element.weather[0].icon}.png" class="w-14 max-w-16 mt-1.5" alt="weather-icon">
                 <h4 class="font-medium mt-3">Temp: ${element.main.temp} °C</h4>
